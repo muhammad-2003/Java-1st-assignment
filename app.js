a92256a65e3dd79cbeadb3e6bbd9cf53b97c3e70
muhammad-2003 <muhammmadm2003@gmail.com>
2023-09-01 23:00:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+
+// . Write a script to greet your website visitor using JS alert 
+// box.
+
+// alert ("Welcome to Page");
+
+// Write a script to display following message on your web 
+// page:
+
+// alert ("Error! Please enter a valid password");
+
+// Write a script to display following message on your web 
+// page: (Hint : Use line break)
+
+// alert ("Welcome to Page \nHappy Coding");
+
+// Declare a variable called username
+
+// var username;
+
+// Declare a variable called myName & assign to it a string 
+// that represents your Full Name.
+
+// var myName = "Muhammad";
+// alert (myName);
+
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box
+
+// var message = "Hello World"
+// alert (message);
+
+// Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+
+// var myName2 = "Jhone Doe";
+// var age = "15 years old";
+// var work = "Certified Mobile Application Developer";
+
+// alert (myName2);
+// alert (age);
+// alert (work);
+
+// Write a script to display the following alert using one JS 
+// variable:
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert (pizza);
+
+// Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation)
+
+// var email = "muhammad2003@gmail.com";
+// alert ("My email address is " +email)
+
+// Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box
+
+var book = "A smarter way to learn JavaScript";
+alert ("I am learning Javascript from book " +book)
